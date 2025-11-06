7a230db83a9c4874f455a8d59e1f8604affc8bb0
Rodrigo Amaro e Silva <rasilva@fc.ul.pt>
2025-11-06 09:33:37 +0000

    Deletes weather inputs before storing SPV output for better RAM use.

--- c3s-lot2-spv-conversion/c3s_lot2_spv_conversion/interface.py
+++ c3s-lot2-spv-conversion/c3s_lot2_spv_conversion/interface.py
@@ -288,6 +288,7 @@ def compute_spv(
     # meshgrid, can be useful for plotting
     XX, YY = np.meshgrid(meta["vLon"], meta["vLat"])
 
+    del (SSRD, T2M)
     if to_save:
         if isinstance(out_path, str):
             # creates and encodes output .nc file
